051f0a54d85d59ad346fd00f079407aaa0133cc6
Lívia <liviamarcelasouza@hotmail.com>
2023-01-26 20:46:38 -0300

    Initial Project

---
+++ frontend/pages/auth-page-ssr.js
@@ -0,0 +1,12 @@
+function AuthPageSSR(props) {
+    return (
+        <div>
+            <h1>
+                Auth Page Server Side Render
+            </h1>
+            
+        </div>
+    )
+}
+
+export default AuthPageSSR
---
+++ frontend/pages/auth-page-static.js
@@ -0,0 +1,12 @@
+function AuthPagestatic(props) {
+    return (
+        <div>
+            <h1>
+                Auth Page Static
+            </h1>
+            
+        </div>
+    )
+}
+
+export default AuthPagestatic
